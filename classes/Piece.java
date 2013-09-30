d582b93d467ddadadcdad7d144aa78863f179735
Corbin McNeill <corbin.mc96@gmail.com>
2013-09-30 12:41:58 -0400

    implemented getAllMoces()

--- classes/Piece.java
+++ classes/Piece.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Piece {
 
 	private boolean isKing;
@@ -30,17 +32,27 @@ public class Piece {
 		return this.owningPlayer;
 	}
 
-	public Move[] getMovesOfPiece() {
-		this.getMovesOfPiece(new byte[][] {this.location}, false);
+	public Move[] getMovesOfPiece () {
+		byte[][][] allWaypoints = this.getMovesFromLocation(this.location, false);
 	}
 
-	public Move[] getMovesOfPiece(byte[][] waypointStack, boolean mustBeJump) {
+	public byte[][][] getMovesFromLocation (byte[] pieceLocation, boolean mustBeJump) {
+		ArrayList<byte[][][]> result = new ArrayList<byte[][][]>();
 		if (this.isKing) {
-			for (byte[] displacement : new byte[][] {new byte[]{1,1}, new byte[] {1,-1}, new byte[] {-1,1}, new byte[] {}
-		}
-		else {
 			if (!mustBeJump) {
-				for byte[] displacement // still working here -- Corbin
+				for (displacement : new byte[][] {new byte[] {1,1}, new byte[] {1,-1}, new byte[] {-1,1}, new byte[] {-1,-1}}) {
+					byte[] testDestination = new byte[] {pieceLocation[0]+displacement[0], pieceLocation[1]+displacement[1]};
+					if (this.owningPlayer.myBoard.getPieceAtLocation(testDestination) == null) {
+						result.add(new byte[][] {this.location,testDestination});
+					}
+				}
+			}
+			for (displacement : new byte[][] {new byte[] {2,2}, new byte[] {2,-2}, new byte[] {-2,2}, new byte[] {-2,-2}}) {
+				byte[] testDestination = new byte[] {pieceLocation[0]+displacement[0], pieceLocation[1]+displacement[1]};
+				byte[] midpoint = new byte[] {pieceLocation[0]+displacement[0]/2, pieceLocation[1]+displacement[1]/2}
+				if (this.owningPlayer.myBoard.getPieceAtLocation(testDestination) == null && this.owningPlayer.myBoard.getPieceAtLocation() != null && this.owningPlayer.myBoard.getPieceAtLocation().owningPlayer != this.owningPlayer) {
+					result.add(new byte[][] {this.locaation,testDestination}) // still working here -- Corbin
+				}
 			}
 		}
 	}
