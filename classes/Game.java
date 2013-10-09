aecc4fa11b15976c59152fddf49acb94864cec53
jphd95 <jamesdingwall@Jamess-MacBook-Pro.local>
2013-10-09 10:21:50 -0400

    Worked on stuff

--- classes/Game.java
+++ classes/Game.java
@@ -20,8 +20,8 @@ public class Game {
 
 	public Player play() {
 		while (this.gameBoard.totalPiecesLeft(this.player1)>0 && this.gameBoard.totalPiecesLeft(this.player2)>0) {
-			this.player1.takeTurn();
-			this.player2.takeTurn();
+			this.player1.takeTurn(this);
+			this.player2.takeTurn(this);
 		}
 		if (this.gameBoard.totalPiecesLeft(player1)>0) {
 			return this.player1;
