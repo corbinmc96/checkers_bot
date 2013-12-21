c2a1cb1e4141303d5ff179271f351cf9320edbf1
Corbin McNeill <corbin.mc96@gmail.com>
2013-12-21 16:38:11 -0500

    added blog adress to README.md

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ CheckersBot Project.
 ============
 
 2014 Wilmington Christian School Senior Computer Programming Project
+
+For project information visit: wcsat.tumblr.com
--- classes/Starter.java
+++ classes/Starter.java
@@ -1,7 +1,7 @@
 public class Starter {
 
 	public static void main(String[] args) {
-		Game theGame = new Game(new SimPlayer("x",true,new CorbinAI()), new Human("o",false,new CorbinAI()));
+		Game theGame = new Game(new SimPlayer("x",true,new AaronAI()), new SimPlayer("o",false,new AaronAI()));
 		Player winner = theGame.play();
 		if (winner == null) {
 			System.out.println("TIE");
