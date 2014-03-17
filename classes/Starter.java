6b057ffdc92ffb553404fb57030dacf84a4ee5e9
Aaron Miller <acmiller273@gmail.com>
2014-03-16 22:31:40 -0400

    added options to standard.sh and Starter.java to prepare for Charva implementation

--- classes/Starter.java
+++ classes/Starter.java
@@ -1,7 +1,7 @@
 public class Starter {
 
 	public static void main(String[] args) {
-		Game theGame = new Game(new SimPlayer("x",true,new AaronAI()), new Human("o",false,new AaronAI()), false);
+		Game theGame = new Game(new SimPlayer("x",true,new MultithreadedAI()), new SimPlayer("o",false,new MultithreadedAI()), (args.length>0 && args[0].equals("official")));
 		Player winner = theGame.play();
 		if (winner == null) {
 			System.out.println("TIE");
--- classes/standard.sh
+++ classes/standard.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-rm *.class
-javac Starter.java
-java Starter
+rm -f *.class
+
+if [ "$1" = "tui" ]; then
+	export LD_LIBRARY_PATH="${2}/c/lib"
+	javac \
+		-cp ".:${2}:${2}/java/classes:${2}/test/classes:${2}/java/lib/commons-logging.jar:${2}/java/lib/log4j-1.2.8.jar" \
+		TUIStarter.java
+	java -Dcharva.color=1 \
+		-cp ".:${2}:${2}/java/classes:${2}/test/classes:${2}/java/lib/commons-logging.jar:${2}/java/lib/log4j-1.2.8.jar" \
+		TUIStarter
+else
+	javac Starter.java
+	java Starter ${1}
+fi
