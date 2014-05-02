30cf68c62394a69a0f7730a3ad24badc616e85be
Aaron Miller <acmiller273@gmail.com>
2014-05-01 23:43:29 -0400

    added main method to Robot.java for testing

--- classes/Robot.java
+++ classes/Robot.java
@@ -3,7 +3,7 @@
 import lejos.pc.comm.*;
 import lejos.nxt.*;
 import lejos.nxt.remote.*;
-import java.io.IOException;
+import java.io.*;
 
 public class Robot {
 
@@ -20,6 +20,107 @@ public class Robot {
 		this.conn = new NXTConnector();
 		this.conn.connectTo(NXTComm.LCP);
 		NXTCommandConnector.setNXTCommand(new NXTCommand(this.conn.getNXTComm()));
+	}
+
+	public static void main(String[] args) throws NXTCommException {
+		Robot r = new Robot();
+
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			boolean done = false;
+			String locationString;
+
+			while (!done) {
+				System.out.println("");
+				System.out.println("0\tgetArmLocation");
+				System.out.println("1\tgetIsHoldingPiece");
+				System.out.println("2\tmoveToXY");
+				System.out.println("3\tresetPosition");
+				System.out.println("4\texamineLocation");
+				System.out.println("5\tpickUpPiece");
+				System.out.println("6\tdropPiece");
+				System.out.println("7\twaitForSensorPress");
+				System.out.println("8\tQUIT");
+
+				System.out.print("Execute: ");
+
+				int input = -1;
+				while (input == -1) {
+					try {
+						input = Integer.parseInt(br.readLine());
+					} catch (IOException e) {
+						System.err.println(e);
+					}
+					if (input<0 || input>8) {
+						input = -1;
+					}
+				}
+
+				switch (input) {
+					case 0:
+						System.out.println("[" + r.getArmLocation()[0] + ", " + r.getArmLocation()[1] + "]");
+						break;
+					case 1:
+						System.out.println(r.getIsHoldingPiece());
+						break;
+					case 2:
+						locationString = null;
+						while (locationString==null) {
+							System.out.print("Enter the location(two integers separated by spaces): ");
+							try {
+								locationString = br.readLine();
+							} catch (IOException e) {
+								System.err.println(e);
+							}
+							if (!locationString.matches("[0-7] [0-7]")) {
+								locationString = null;
+							}
+						}
+						r.moveToXY(new int[] {Integer.parseInt(locationString.substring(0, 1)), Integer.parseInt(locationString.substring(2))});
+						break;
+					case 3:
+						r.resetPosition();
+						break;
+					case 4:
+						locationString = null;
+						while (locationString==null) {
+							System.out.print("Enter the location(two integers separated by spaces): ");
+							try {
+								locationString = br.readLine();
+							} catch (IOException e) {
+								System.err.println(e);
+							}
+							if (!locationString.matches("[0-7] [0-7]")) {
+								locationString = null;
+							}
+						}
+						System.out.println(r.examineLocation(new int[] {Integer.parseInt(locationString.substring(0, 1)), Integer.parseInt(locationString.substring(2))}));
+						break;
+					case 5:
+						r.pickUpPiece();
+						break;
+					case 6:
+						r.dropPiece();
+						break;
+					case 7:
+						r.waitForSensorPress();
+						break;
+					case 8:
+						done = true;
+						break;
+					default:
+						System.err.println("Something went wrong!");
+						break;
+				}
+			}
+		} finally {
+			try {
+				r.disconnect();
+			} catch (IOException e) {
+				System.err.println(e);
+			}
+		}
 	}
 
 	public void disconnect() throws IOException {
